aaef48ae012e0df41508343f1d49c1edda864253
Nikiticko <nikitasemenenko3@gmail.com>
2024-10-02 18:27:09 +0300

    add 3 pages and connect

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var app = express()
 app.get("/",function(req,res){
 res.send("Серверная информация для браузера")
 })
+app.set('view engine', 'ejs');
+
 app.listen("3000")
 const mysql = require("mysql2");
-  
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  database: "",
+  database: "driving_school",
   password: "6969"
 });
  connection.connect(function(err){
@@ -20,3 +21,11 @@ const connection = mysql.createConnection({
       console.log("Подключение к серверу MySQL успешно установлено");
     }
  });
+
+ // Указываем папку с js, css и картинками
+app.use(express.static('public'));
+
+// Связываем адреса и их страницы
+app.get('/main', (req, res) => res.render('main'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/dashboard', (req, res) => res.render('dashboard'));
